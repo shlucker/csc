3eb5a9b78d37e6227c55771723aca0420d0276e0
stenci <stefano.menci@gmail.com>
2017-01-07 17:01:40 -0600

    More test data and more queries

--- test.py
+++ test.py
@@ -9,14 +9,14 @@ def import_from_excel():
     book = xlrd.open_workbook('test.xlsx')
 
     sheet = book.sheet_by_name('School')
-    for r in range(sheet.nrows):
+    for r in range(1, sheet.nrows):
         name = sheet.cell(r, 0).value
         city = sheet.cell(r, 1).value
         state = sheet.cell(r, 2).value
         School(name=name, city=city, state=state)
 
     sheet = book.sheet_by_name('Member')
-    for r in range(sheet.nrows):
+    for r in range(1, sheet.nrows):
         name = sheet.cell(r, 0).value
         major = sheet.cell(r, 1).value
         city = sheet.cell(r, 2).value
@@ -26,13 +26,22 @@ def import_from_excel():
         Member(name=name, major=major, state=state, city=city, email=email, school=school)
 
     sheet = book.sheet_by_name('Club')
-    for r in range(sheet.nrows):
+    for r in range(1, sheet.nrows):
         name = sheet.cell(r, 0).value
         school = School.get(name=sheet.cell(r, 1).value)
-        Club(name=name, school=school)
+        president = sheet.cell(r, 2).value
+        vicepresident = sheet.cell(r, 3).value
+        treasurer = sheet.cell(r, 4).value
+        secretary = sheet.cell(r, 5).value
+        president = Member.get(name=president) if president else None
+        vicepresident = Member.get(name=vicepresident) if vicepresident else None
+        treasurer = Member.get(name=treasurer) if treasurer else None
+        secretary = Member.get(name=secretary) if secretary else None
+        Club(name=name, school=school, president=president, vicepresident=vicepresident, treasurer=treasurer,
+             secretary=secretary)
 
     sheet = book.sheet_by_name('Company')
-    for r in range(sheet.nrows):
+    for r in range(1, sheet.nrows):
         name = sheet.cell(r, 0).value
         city = sheet.cell(r, 1).value
         state = sheet.cell(r, 2).value
@@ -40,7 +49,7 @@ def import_from_excel():
         Company(name=name, city=city, state=state, email=email)
 
     sheet = book.sheet_by_name('CompetitionHost')
-    for r in range(sheet.nrows):
+    for r in range(1, sheet.nrows):
         name = sheet.cell(r, 0).value
         city = sheet.cell(r, 1).value
         state = sheet.cell(r, 2).value
@@ -60,3 +69,10 @@ if __name__ == '__main__':
         print('== Get member by name ==')
         u1 = User.get(name='Sade Berney')
         print(u1.name, u1.major, u1.city, u1.state)
+
+        print('== Get members by school ==')
+        for s in ponyorm.select(s for s in School):
+            if s.members:
+                print('Members of school: {}'.format(s.name))
+                for m in s.members:
+                    print(' ', m.name)
