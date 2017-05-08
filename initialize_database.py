f4d2e9a0f6ff5ab12f5ba496207aa082ca54c065
stenci <stefano.menci@gmail.com>
2017-05-07 23:23:31 -0500

    initialize_database uses column headers instead of number

--- initialize_database.py
+++ initialize_database.py
@@ -16,13 +16,20 @@ class TestTable:
     def __init__(self, fname):
         self.tables = {}
         sh_name = ''
+        headers = None
+        headers_are_coming = False
         for row in open(fname, 'r'):
             row = row[:-1]
             if row.startswith('==') and row.endswith('=='):
                 sh_name = row[3:-3]
                 self.tables[sh_name] = []
+                headers_are_coming = True
+            elif headers_are_coming:
+                headers = [cell.rstrip() for cell in row.split('|')]
+                headers_are_coming = False
             elif row:
-                self.tables[sh_name].append([cell.rstrip() for cell in row.split('|')])
+                self.tables[sh_name].append(
+                    {header: cell.rstrip() for (header, cell) in zip(headers, row.split('|'))})
 
     def __getitem__(self, table_name):
         return self.tables[table_name]
@@ -31,7 +38,7 @@ class TestTable:
         return len(self.tables[table_name])
 
     def n_columns(self, table_name):
-        return len(self.tables[table_name][0])
+        return len(self.tables[table_name].items()[0])
 
 
 @orm.db_session
@@ -39,92 +46,96 @@ def import_from_excel():
     test_table = TestTable('test.txt')
 
     print('Creating schools')
-    for r in range(1, test_table.n_rows('School')):
-        name = test_table['School'][r][0]
-        city = test_table['School'][r][1]
-        state = test_table['School'][r][2]
+    for r in range(test_table.n_rows('School')):
+        name = test_table['School'][r]['name']
+        city = test_table['School'][r]['city']
+        state = test_table['School'][r]['state']
         models.School(name=name, city=city, state=state)
 
     print('Creating administrators')
-    for r in range(1, test_table.n_rows('Administrator')):
-        name = test_table['Administrator'][r][0]
-        email = test_table['Administrator'][r][1]
-        password = hash_password(test_table['Administrator'][r][2])
-        city = test_table['Administrator'][r][3]
-        state = test_table['Administrator'][r][4]
+    for r in range(test_table.n_rows('Administrator')):
+        name = test_table['Administrator'][r]['name']
+        email = test_table['Administrator'][r]['email']
+        password = hash_password(test_table['Administrator'][r]['password'])
+        city = test_table['Administrator'][r]['city']
+        state = test_table['Administrator'][r]['state']
         models.Administrator(name=name, email=email, password=password, state=state, city=city)
 
     print('Creating members')
-    for r in range(1, test_table.n_rows('Member')):
-        name = test_table['Member'][r][0]
-        email = test_table['Member'][r][1]
-        password = hash_password(test_table['Member'][r][2])
-        major = test_table['Member'][r][3]
-        city = test_table['Member'][r][4]
-        state = test_table['Member'][r][5]
-        school = models.School[int(test_table['Member'][r][6])]
+    for r in range(test_table.n_rows('Member')):
+        name = test_table['Member'][r]['name']
+        email = test_table['Member'][r]['email']
+        password = hash_password(test_table['Member'][r]['password'])
+        major = test_table['Member'][r]['major']
+        city = test_table['Member'][r]['city']
+        state = test_table['Member'][r]['state']
+        school = models.School[int(test_table['Member'][r]['school'])]
         models.Member(name=name, major=major, state=state, city=city, email=email, password=password, school=school)
 
     print('Creating clubs')
-    for r in range(1, test_table.n_rows('Club')):
-        name = test_table['Club'][r][0]
-        school = models.School[int(test_table['Club'][r][1])]
-        president = models.Member[int(test_table['Club'][r][2])] if test_table['Club'][r][2] else None
-        vicepresident = models.Member[int(test_table['Club'][r][3])] if test_table['Club'][r][3] else None
-        treasurer = models.Member[int(test_table['Club'][r][4])] if test_table['Club'][r][4] else None
-        secretary = models.Member[int(test_table['Club'][r][5])] if test_table['Club'][r][5] else None
-        members = [models.Member[i] for i in test_table['Club'][r][6].split()]
+    for r in range(test_table.n_rows('Club')):
+        name = test_table['Club'][r]['name']
+        school = models.School[int(test_table['Club'][r]['school'])]
+        president = models.Member[int(test_table['Club'][r]['president'])] if test_table['Club'][r][
+            'president'] else None
+        vicepresident = models.Member[int(test_table['Club'][r]['vicepresident'])] if test_table['Club'][r][
+            'vicepresident'] else None
+        treasurer = models.Member[int(test_table['Club'][r]['treasurer'])] if test_table['Club'][r][
+            'treasurer'] else None
+        secretary = models.Member[int(test_table['Club'][r]['secretary'])] if test_table['Club'][r][
+            'secretary'] else None
+        members = [models.Member[i] for i in test_table['Club'][r]['members'].split()]
         models.Club(name=name, school=school, president=president, vicepresident=vicepresident, treasurer=treasurer,
                     secretary=secretary, members=members)
 
     print('Creating companies')
-    for r in range(1, test_table.n_rows('Company')):
-        name = test_table['Company'][r][0]
-        email = test_table['Company'][r][1]
-        password = hash_password(test_table['Company'][r][2])
-        city = test_table['Company'][r][3]
-        state = test_table['Company'][r][4]
+    for r in range(test_table.n_rows('Company')):
+        name = test_table['Company'][r]['name']
+        email = test_table['Company'][r]['email']
+        password = hash_password(test_table['Company'][r]['password'])
+        city = test_table['Company'][r]['city']
+        state = test_table['Company'][r]['state']
         models.Company(name=name, email=email, password=password, city=city, state=state)
 
     print('Creating competition hosts')
-    for r in range(1, test_table.n_rows('CompetitionHost')):
-        name = test_table['CompetitionHost'][r][0]
-        email = test_table['CompetitionHost'][r][1]
-        password = hash_password(test_table['CompetitionHost'][r][2])
-        city = test_table['CompetitionHost'][r][3]
-        state = test_table['CompetitionHost'][r][4]
+    for r in range(test_table.n_rows('CompetitionHost')):
+        name = test_table['CompetitionHost'][r]['name']
+        email = test_table['CompetitionHost'][r]['email']
+        password = hash_password(test_table['CompetitionHost'][r]['password'])
+        city = test_table['CompetitionHost'][r]['city']
+        state = test_table['CompetitionHost'][r]['state']
         models.CompetitionHost(name=name, email=email, password=password, city=city, state=state)
 
     print('Creating competitions')
-    for r in range(1, test_table.n_rows('Competition')):
-        name = test_table['Competition'][r][0]
-        description = test_table['Competition'][r][1]
-        date = test_table['Competition'][r][2]
-        website = test_table['Competition'][r][3]
-        clubs = [models.Club[i] for i in test_table['Competition'][r][4].split()]
-        competition_host = models.CompetitionHost[int(test_table['Competition'][r][5])]
-        members = [models.Member[i] for i in test_table['Competition'][r][6].split()]
+    for r in range(test_table.n_rows('Competition')):
+        name = test_table['Competition'][r]['name']
+        description = test_table['Competition'][r]['description']
+        date = test_table['Competition'][r]['date']
+        website = test_table['Competition'][r]['website']
+        clubs = [models.Club[i] for i in test_table['Competition'][r]['clubs'].split()]
+        competition_host = models.CompetitionHost[int(test_table['Competition'][r]['competition_host'])]
+        members = [models.Member[i] for i in test_table['Competition'][r]['members'].split()]
         models.Competition(name=name, description=description, date=date, website=website, clubs=clubs,
                            competition_host=competition_host, members=members)
 
     print('Creating notifications')
-    for r in range(1, test_table.n_rows('Notification')):
-        from_competition = models.Competition[int(test_table['Notification'][r][0])] if test_table['Notification'][r][
-            0] else None
-        from_job_offer = models.JobOffer[int(test_table['Notification'][r][1])] if test_table['Notification'][r][
-            1] else None
-        to_users = [models.Member[i] for i in test_table['Notification'][r][2].split()]
-        to_clubs = [models.Club[i] for i in test_table['Notification'][r][3].split()]
-        date = test_table['Notification'][r][4]
+    for r in range(test_table.n_rows('Notification')):
+        from_competition = models.Competition[int(test_table['Notification'][r]['from_competition'])] if \
+            test_table['Notification'][r]['from_competition'] else None
+        from_job_offer = models.JobOffer[int(test_table['Notification'][r]['from_job_offer'])] if \
+            test_table['Notification'][r]['from_job_offer'] else None
+        to_users = [models.Member[i] for i in test_table['Notification'][r]['to_users'].split()]
+        to_clubs = [models.Club[i] for i in test_table['Notification'][r]['to_clubs'].split()]
+        date = test_table['Notification'][r]['date']
         models.Notification(from_competition=from_competition, from_job_offer=from_job_offer, to_users=to_users,
                             to_clubs=to_clubs, date=date)
 
     print('Creating posts')
-    for r in range(1, test_table.n_rows('Post')):
-        title = test_table['Post'][r][0]
-        text = test_table['Post'][r][1]
-        date = test_table['Post'][r][2]
-        notification = models.Notification[int(test_table['Post'][r][3])]
+    for r in range(test_table.n_rows('Post')):
+        title = test_table['Post'][r]['title']
+        text = test_table['Post'][r]['text']
+        date = test_table['Post'][r]['date']
+        notification = models.Notification[int(test_table['Post'][r]['notification'])]
         models.Post(title=title, text=text, date=date, notification=notification)
 
 
